1627774dbef7d2451d1d35a9e27ab85722f417fd
Andrey999 <1985gromovandrey@gmail.com>
2018-02-05 15:03:22 +0300

    final_commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,32 @@
 $(document).ready(function () {
    $('.slider').slick();
 });
+
+
+
+
+
+
+
+
+
+
+$(function() {
+    $(".dev-des-box__btn").bind('touchend click',function(event){
+        event.preventDefault();
+        var boxTarget = '.dev-des-box__' + $(this).attr('data-target');
+        $('.dev-des-box__tab').removeClass('active');
+        $(boxTarget).addClass('active');
+    });
+
+    $(".ddb-open-popup").on('click touchend',function(event){
+
+        var boxTarget = '#' + $(this).attr('data-popup');
+        $(boxTarget).addClass('active-pop');
+    });
+    $(".ddb-close-popup").on('click touchend',function(event){
+
+        var boxTarget = '#' + $(this).attr('data-popup');
+        $(boxTarget).removeClass('active-pop');
+    });
+});
